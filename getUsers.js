0747bffb8ab556afc6d2ef5d4662e19fa3a3f001
ChadLutz <56328442+ChadLutz@users.noreply.github.com>
2025-06-17 20:43:24 -0700

    Final commit, updating sessionid's

--- getUsers.js
+++ getUsers.js
@@ -6,7 +6,7 @@ const authHeaders = {
     'Content-Type': 'application/x-www-form-urlencoded',
     'Accept': 'application/json',
     'User-Agent': 'Chad Lutz Challenge',
-    'Cookie': 'JSESSIONID=2d4a5408-7840-4136-b033-eda5f5e22512; _csrf_token=5a1cf6ac19c20abd51f276645386833771eb25b6a75c429cc5c3632f62341a2f; analytics_id=analytics_74e3c65b34275066a84ad4e011034723; device_id=device_9f4ef74dde1416c34a2e1fd7; feature_flags=eyJuZXdEYXNoYm9hcmQiOnRydWUsImJldGFGZWF0dXJlcyI6ZmFsc2UsImFkdmFuY2VkU2V0dGluZ3MiOnRydWUsImV4cGVyaW1lbnRhbFVJIjpmYWxzZX0%3D; session_fingerprint=2de6f07e5e406323caedd34932fc44d14d8fd9027ebc4a849b468f8a32bbb6cc; tracking_consent=accepted; user_preferences=eyJ0aGVtZSI6ImxpZ2h0IiwibGFuZ3VhZ2UiOiJlbiIsInRpbWV6b25lIjoiVVRDIiwibm90aWZpY2F0aW9ucyI6dHJ1ZX0%3D'
+    'Cookie': 'JSESSIONID=412e55ec-a6f2-4eed-b90c-c3209197e987; _csrf_token=6978e07753f189f6c54830c5224c8a49442d6262f10a0f83564b2553e44743fd; user_preferences=eyJ0aGVtZSI6ImxpZ2h0IiwibGFuZ3VhZ2UiOiJlbiIsInRpbWV6b25lIjoiVVRDIiwibm90aWZpY2F0aW9ucyI6dHJ1ZX0%3D; analytics_id=analytics_bcf023996b45b74da28f18d52cacb2b4; session_fingerprint=37c4191508d1f0199f763daae3034306cc410caefbd877c63e0c6b929ae55d73; feature_flags=eyJuZXdEYXNoYm9hcmQiOnRydWUsImJldGFGZWF0dXJlcyI6ZmFsc2UsImFkdmFuY2VkU2V0dGluZ3MiOnRydWUsImV4cGVyaW1lbnRhbFVJIjpmYWxzZX0%3D; tracking_consent=accepted; device_id=device_c9fa91c862c51c828875b833'
 };
 
 function fetch(host, path, postData , callMethod, callback) {
@@ -73,7 +73,6 @@ function fetch(host, path, postData , callMethod, callback) {
         checkcode: checkcode
     }
     const params = new URLSearchParams(fullPostData).toString();
-    console.log(params);
 
   fetch('api.challenge.sunvoy.com', '/api/settings', params ,  'POST' , (err, currentUserData) => {
         if (err) return console.error('Error fetching current user:', err.message);
